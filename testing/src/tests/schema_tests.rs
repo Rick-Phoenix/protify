1e608c31be12689a4eea7c3cde8fef6916a3ce0e
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2026-01-09 18:30:17 +0100

    added test for int wrappers resolution

--- testing/src/tests/schema_tests.rs
+++ testing/src/tests/schema_tests.rs
@@ -2,3 +2,42 @@ use super::*;
 mod deprecated_tests;
 mod nested_items_tests;
 mod rendering_tests;
+
+#[proto_message(no_auto_test)]
+struct IntWrappers {
+  #[proto(sint32)]
+  sint32: i32,
+  #[proto(sint64)]
+  sint64: i64,
+  #[proto(sfixed32)]
+  sfixed32: i32,
+  #[proto(sfixed64)]
+  sfixed64: i64,
+  #[proto(fixed32)]
+  fixed32: u32,
+  #[proto(fixed64)]
+  fixed64: u64,
+}
+
+#[test]
+fn int_wrappers_resolution() {
+  let schema = IntWrappers::proto_schema();
+
+  let outputs = [
+    ProtoScalar::Sint32,
+    ProtoScalar::Sint64,
+    ProtoScalar::Sfixed32,
+    ProtoScalar::Sfixed64,
+    ProtoScalar::Fixed32,
+    ProtoScalar::Fixed64,
+  ];
+
+  for (field, exp_type) in schema.fields().zip(outputs) {
+    assert_eq_pretty!(
+      field.type_,
+      FieldType::Normal(ProtoType::Scalar(exp_type)),
+      "expected field {} to have the type {exp_type}",
+      field.name
+    );
+  }
+}
